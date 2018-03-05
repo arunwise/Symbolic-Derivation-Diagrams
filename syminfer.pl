fcb2865247a40783e06fa9024f5007761d513d9f
Arun Nampally <anampally@cs.stonybrook.edu>
2018-03-05 11:11:50 -0500

    refactor and move visualization to separate module

--- syminfer.pl
+++ syminfer.pl
@@ -9,6 +9,8 @@
 :- import list_to_ord_set/2 from ordsets.
 :- import empty_assoc/1, put_assoc/4, get_assoc/3, list_to_assoc/2 from assoc_xsb.
 
+:- import writeDotFile/2 from visualize.
+
 :- install_verify_attribute_handler(type, AttrValue, Target, type_handler(AttrValue, Target)).
 :- install_verify_attribute_handler(id, AttrValue, Target, id_handler(AttrValue, Target)).
 
@@ -826,71 +828,8 @@ edge_prob_1(R, V, T, P) :-
     P is Pv * Pt.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Visualization using DOT
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% writeDot(OSDD, DotFile) :-
-%%     % paths(OSDD, Paths),
-%%     dotEdges(OSDD, [], Edges),
-%%     current_prolog_flag(write_attributes, F),
-%%     set_prolog_flag(write_attributes, ignore),
-%%     open(DotFile, write, Handle),
-%%     % write(Handle, 'strict digraph osdd {\n'),
-%%     write(Handle, 'digraph osdd {\n'),
-%%     % writeDotPaths(Paths, Handle),
-%%     writeDotEdges(Edges, Handle),
-%%     write(Handle, '}\n'),
-%%     close(Handle),
-%%     set_prolog_flag(write_attributes, F).
-
-writeDot(OSDD, DotFile) :-
-    current_prolog_flag(write_attributes, F),
-    set_prolog_flag(write_attributes, ignore),
-    open(DotFile, write, Handle),
-%    write(Handle, 'strict digraph osdd {\n'),
-    write(Handle, ' digraph osdd {\n'),
-    traverse(OSDD, Handle),
-    write(Handle, '}\n'),
-    close(Handle),
-    set_prolog_flag(write_attributes, F).
-
-traverse(T, Handle) :-
-    traverse(T, _, Handle).
-
-traverse(leaf(X), ID, Handle) :-
-    write(Handle, ID),
-    write(Handle, ' [label='),
-    write(Handle, X),
-    write(Handle, '];\n').
-traverse(tree(R, Es), ID, Handle) :-
-    write(Handle, ID), write(Handle, ' [label='), write(Handle, R), write(Handle, '];\n'),
-    traverse_edges(Es, ID, Handle).
-traverse_edges([], _, _).
-traverse_edges([edge_subtree(C,T)|Es], ParentID, Handle) :-
-    ChildID=_, % fresh
-    write(Handle, ParentID), write(Handle, ' -> '), write(Handle, ChildID),
-    write(Handle, ' [label='), write(Handle, '"'),writeDotConstraint(Handle, C), write(Handle, '"'), write(Handle, '];\n'),
-    traverse(T, ChildID, Handle),
-    traverse_edges(Es, ParentID, Handle).
-
-writeDotConstraint(Handle, null) :-
-    write(Handle, '').
-writeDotConstraint(Handle, []) :-
-    write(Handle, '').
-writeDotConstraint(Handle, [C]) :-
-    write1(Handle, C).
-writeDotConstraint(Handle, [C|R]) :-
-    R \= [],
-    write1(Handle, C), write(Handle, ','),
-    writeDotConstraint(Handle, R).
-
-write1(Handle, X=Y) :-
-    write(Handle, X=Y).
-write1(Handle, X\=Y) :-
-    write(Handle, X), write(Handle, '\\\='), write(Handle, Y).
-write1(Handle, X<Y) :-
-    write(Handle, X<Y).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Misc
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 display_attributes(on).  % control display of attributes
+
+writeDot(OSDD, File) :- writeDotFile(OSDD, File).
---
+++ visualize.pl
@@ -0,0 +1,53 @@
+:- export writeDotFile/2.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Visualization using DOT
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+writeDotFile(OSDD, DotFile) :-
+    current_prolog_flag(write_attributes, F),
+    set_prolog_flag(write_attributes, ignore),
+    open(DotFile, write, Handle),
+%    write(Handle, 'strict digraph osdd {\n'),
+    write(Handle, ' digraph osdd {\n'),
+    traverse(OSDD, Handle),
+    write(Handle, '}\n'),
+    close(Handle),
+    set_prolog_flag(write_attributes, F).
+
+traverse(T, Handle) :-
+    traverse(T, _, Handle).
+
+traverse(leaf(X), ID, Handle) :-
+    write(Handle, ID),
+    write(Handle, ' [label='),
+    write(Handle, X),
+    write(Handle, '];\n').
+traverse(tree(R, Es), ID, Handle) :-
+    write(Handle, ID), write(Handle, ' [label='), write(Handle, R), write(Handle, '];\n'),
+    traverse_edges(Es, ID, Handle).
+traverse_edges([], _, _).
+traverse_edges([edge_subtree(C,T)|Es], ParentID, Handle) :-
+    ChildID=_, % fresh
+    write(Handle, ParentID), write(Handle, ' -> '), write(Handle, ChildID),
+    write(Handle, ' [label='), write(Handle, '"'),writeDotConstraint(Handle, C), write(Handle, '"'), write(Handle, '];\n'),
+    traverse(T, ChildID, Handle),
+    traverse_edges(Es, ParentID, Handle).
+
+writeDotConstraint(Handle, null) :-
+    write(Handle, '').
+writeDotConstraint(Handle, []) :-
+    write(Handle, '').
+writeDotConstraint(Handle, [C]) :-
+    write1(Handle, C).
+writeDotConstraint(Handle, [C|R]) :-
+    R \= [],
+    write1(Handle, C), write(Handle, ','),
+    writeDotConstraint(Handle, R).
+
+write1(Handle, X=Y) :-
+    write(Handle, X=Y).
+write1(Handle, X\=Y) :-
+    write(Handle, X), write(Handle, '\\\='), write(Handle, Y).
+write1(Handle, X<Y) :-
+    write(Handle, X<Y).
