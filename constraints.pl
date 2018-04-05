5a78fd0c7cd0e4fda8cbae27cbb73e23fecf751e
Arun Nampally <anampally@cs.stonybrook.edu>
2018-04-04 21:47:53 -0400

    change computation of canonical representation of constriant formula

--- constraints.pl
+++ constraints.pl
@@ -4,12 +4,14 @@ canonical_constraint/3.
 :- import is_empty/1, delete/3 from lists.
 
 :- import vertices_edges_to_ugraph/3, transitive_closure/2, edges/2,
-add_edges/3, del_edges/3 from ugraphs.
+add_edges/3, del_edges/3, vertices/2 from ugraphs.
 
 :- import empty_assoc/1, put_assoc/4, gen_assoc/3, assoc_to_list/2
 from assoc_xsb.
 
 :- import list_to_ord_set/2 from ordsets.
+
+:- import append/3, member/2 from basics.
 
 :- import (in)/2, (#=)/2, (#\=)/2, label/1 from bounds.
 
@@ -76,7 +78,7 @@ enforce_domain_constraints([Key-Value|Rest]) :-
 	enforce_domain_constraints(Rest)
     ;
         % get the id corresponding to the variable, then its domain
-        usermod:canonical_label(S, _I, Key),
+        usermod:'$canonical_label'(S, _I, Key),
 	usermod:intrange(S, Lower, Upper),
 	Value in Lower..Upper,
 	enforce_domain_constraints(Rest)
@@ -89,13 +91,13 @@ Loop through the S_representation and enforce equality constraints
 specified by edges. The constraints are enforced using bounds package
 and on the variables that are mapped to the labels.
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-enforce_equality_constraints(Assoc, []).
+enforce_equality_constraints(_Assoc, []).
 enforce_equality_constraints(Assoc, [Vertex-Neighbors|Rest]) :-
     gen_assoc(Vertex, Assoc, Value),
     enforce_equality_with_neighbors(Value, Assoc, Neighbors),
     enforce_equality_constraints(Assoc, Rest).
 
-enforce_equality_with_neighbors(Value, Assoc, []).
+enforce_equality_with_neighbors(_Value, _Assoc, []).
 enforce_equality_with_neighbors(Value, Assoc, [H|R]) :-
     gen_assoc(H, Assoc, Value1),
     Value #= Value1,
@@ -108,13 +110,13 @@ Loop through the S_representation and enforce disequality constraints
 specified by edges. The constraints are enforced using bounds package
 and on the variables that are mapped to the labels.
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-enforce_disequality_constraints(Assoc, []).
+enforce_disequality_constraints(_Assoc, []).
 enforce_disequality_constraints(Assoc, [Vertex-Neighbors|Rest]) :-
     gen_assoc(Vertex, Assoc, Value),
     enforce_disequality_with_neighbors(Value, Assoc, Neighbors),
     enforce_disequality_constraints(Assoc, Rest).
 
-enforce_disequality_with_neighbors(Value, Assoc, []).
+enforce_disequality_with_neighbors(_Value, _Assoc, []).
 enforce_disequality_with_neighbors(Value, Assoc, [H|R]) :-
     gen_assoc(H, Assoc, Value1),
     Value #\= Value1,
@@ -132,7 +134,7 @@ solutions(Label, EQ, NEQ, Solutions) :-
     ->
 	% Variable corresponding to label is unconstrained
 	%usermod:'$id_label'(id(S, _I), Label),
-	usermod:canonical_label(S, _I, Label),
+	usermod:'$canonical_label'(S, _I, Label),
 	usermod:intrange(S, Lower, Upper),
 	var(X),
 	X in Lower..Upper,
@@ -155,34 +157,59 @@ solutions(Label, EQ, NEQ, Solutions) :-
     ).
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-canonical_constraint(+EQ, +NEQ, cg(-EQ1, -NEQ1))
+canonical_constraint(+EQ, +NEQ, cg(-EC, -NEC))
 
 Given vertices edges representation of constraint graph in EQ and NEQ,
-complete the equality and disequality constraints, change them to
-S-representation, sort them to get EQ1 and NEQ1. Return cg(EQ1, NEQ1)
-as the canonical representation of the constraint graph.
+complete the equality and disequality constraints, to get EC and
+NEC. Return cg(EC, NEC) as the canonical representation of the
+constraint graph.
+
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-canonical_constraint(EQ, NEQ, cg(G11, G33)) :-
-    % G1 = transitive_closure(EQ)
+canonical_constraint(EQ, NEQ, cg(EC, NEC)) :-
+    % compute completed equality edges using transitive closure
     vertices_edges_to_ugraph([], EQ, EQS),
     transitive_closure(EQS, G1),
-    % G2 = transitive_closure(NEQ)
-    vertices_edges_to_ugraph([], NEQ, NEQS),
-    transitive_closure(NEQS, G2),
-    % G3 = transitive_closure(EQ + NEQ)
-    edges(NEQS, NEQedges),
-    add_edges(EQS, NEQedges, EQNEQ),
-    transitive_closure(EQNEQ, G3),
-    % completed equality edges are in G1
-    % completed disequality edges are in G3 - (G1 + G2)
-    % however we need to remove self loops and duplicates
-    edges(G1, Edges1),
-    edges(G2, Edges2),
-    del_edges(G3, Edges1, G31),
-    del_edges(G31, Edges2, G32),
-    % discard self-loops and duplicate neighbors
     discard_redundant_edges(G1, G11),
-    discard_redundant_edges(G32, G33).
+    edges(G11, EC),
+
+    % compute completed disequality edges
+    vertices_edges_to_ugraph([], NEQ, NEQS),
+    inferred_disequality(EQS, NEQS, [], INEQ),
+    add_edges(NEQS, INEQ, G2),
+    discard_redundant_edges(G2, G22),
+    edges(G22, NEC).
+    
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+inferred_disequality(+EQS, +NEQS, +INEQin -INEQout)
+
+Given equality edges in EQS and disequality edges in NEQS, infer extra
+disequality edges and put them in INEQout.
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+inferred_disequality([], _NEQ, INEQin, INEQin).
+inferred_disequality([V-Neib | Rest], NEQ, INEQin, INEQout) :-
+    vertices(NEQ, NEQV),
+    (member(V, NEQV)
+    ->
+	neighbors(V, NEQ, Neib1),
+	pairwise(Neib, Neib1, INEQ),
+	append(INEQin, INEQ, INEQtmp)
+    ;
+        INEQtmp = INEQin
+    ),
+    inferred_disequality(Rest, NEQ, INEQtmp, INEQout).
+
+pairwise(Neib1, Neib2, Pairs) :-
+    pairwise_1(Neib1, Neib2, [], Pairs).
+
+pairwise_1([], _Neib2, PairsIn, PairsIn).
+pairwise_1([N1| Rest1], Neib2, PairsIn, PairsOut) :-
+    pairwise_2(N1, Neib2, Pairs),
+    append(PairsIn, Pairs, PairsTmp),
+    pairwise_1(Rest1, Neib2, PairsTmp, PairsOut).
+
+pairwise_2(N, [], []).
+pairwise_2(N, [N2 | Rest2], [N-N2, N2-N | Rest]) :-
+    pairwise_2(N, Rest2, Rest).
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 discard_redundant_edges(+S_in, -S_out)
