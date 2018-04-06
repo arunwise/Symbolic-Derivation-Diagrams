1d3b83c724f400b71b67a6ec582f216358fa6ba5
Arun Nampally <anampally@cs.stonybrook.edu>
2018-04-05 22:17:05 -0400

    remove commented code in visualize.pl

--- visualize.pl
+++ visualize.pl
@@ -5,15 +5,11 @@
 :- export writeDotFile/2.
 
 writeDotFile(OSDD, DotFile) :-
-    %% current_prolog_flag(write_attributes, F),
-    %% set_prolog_flag(write_attributes, ignore),
     open(DotFile, write, Handle),
-%    write(Handle, 'strict digraph osdd {\n'),
     write(Handle, ' digraph osdd {\n'),
     traverse(OSDD, Handle),
     write(Handle, '}\n'),
     close(Handle),
-    %% set_prolog_flag(write_attributes, F),
     true.
 
 traverse(T, Handle) :-
@@ -44,20 +40,6 @@ traverse(Osdd, ID, Handle) :-
 	)
     ).
 		
-%% traverse(leaf(X), ID, Handle) :-
-%%     write(Handle, ID),
-%%     write(Handle, ' [label='),
-%%     write(Handle, X),
-%%     write(Handle, '];\n').
-%% traverse(leaf(X), ID, Handle) :-
-%%     write(Handle, ID),
-%%     write(Handle, ' [label='),
-%%     write(Handle, X),
-%%     write(Handle, '];\n').
-
-%% traverse(tree(R, Es), ID, Handle) :-
-%%     write(Handle, ID), write(Handle, ' [label='), write(Handle, R), write(Handle, '];\n'),
-%%     traverse_edges(Es, ID, Handle).
 traverse_edges([], _, _).
 traverse_edges([edge_subtree(C,T)|Es], ParentID, Handle) :-
     ChildID=_, % fresh
@@ -66,8 +48,6 @@ traverse_edges([edge_subtree(C,T)|Es], ParentID, Handle) :-
     traverse(T, ChildID, Handle),
     traverse_edges(Es, ParentID, Handle).
 
-%% writeDotConstraint(Handle, null) :-
-%%     write(Handle, '').
 writeDotConstraint(Handle, []) :-
     write(Handle, '').
 writeDotConstraint(Handle, [C]) :-
